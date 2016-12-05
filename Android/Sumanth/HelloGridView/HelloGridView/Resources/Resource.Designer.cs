b75632c2381c2fec74aa7970e68b76135d5c92c8
sammy58492 <kurellasumanth@hotmail.com>
2016-12-05 12:35:15 +0530

    ReviewPopup changed

--- Android/Sumanth/HelloGridView/HelloGridView/Resources/Resource.Designer.cs
+++ Android/Sumanth/HelloGridView/HelloGridView/Resources/Resource.Designer.cs
@@ -1648,20 +1648,20 @@ namespace HelloGridView
 			// aapt resource value: 0x7f0b0073
 			public const int btnSec = 2131427443;
 			
-			// aapt resource value: 0x7f0b0086
-			public const int btnSubmitReview = 2131427462;
+			// aapt resource value: 0x7f0b0085
+			public const int btnSubmitReview = 2131427461;
 			
 			// aapt resource value: 0x7f0b0071
 			public const int btnWall = 2131427441;
 			
-			// aapt resource value: 0x7f0b0089
-			public const int button = 2131427465;
+			// aapt resource value: 0x7f0b0088
+			public const int button = 2131427464;
 			
 			// aapt resource value: 0x7f0b006d
 			public const int button1 = 2131427437;
 			
-			// aapt resource value: 0x7f0b008a
-			public const int button2 = 2131427466;
+			// aapt resource value: 0x7f0b0089
+			public const int button2 = 2131427465;
 			
 			// aapt resource value: 0x7f0b006e
 			public const int button3 = 2131427438;
@@ -1786,8 +1786,8 @@ namespace HelloGridView
 			// aapt resource value: 0x7f0b000a
 			public const int normal = 2131427338;
 			
-			// aapt resource value: 0x7f0b0088
-			public const int pager = 2131427464;
+			// aapt resource value: 0x7f0b0087
+			public const int pager = 2131427463;
 			
 			// aapt resource value: 0x7f0b002f
 			public const int parentPanel = 2131427375;
@@ -1918,8 +1918,8 @@ namespace HelloGridView
 			// aapt resource value: 0x7f0b0069
 			public const int tableRow8 = 2131427433;
 			
-			// aapt resource value: 0x7f0b0087
-			public const int tabs = 2131427463;
+			// aapt resource value: 0x7f0b0086
+			public const int tabs = 2131427462;
 			
 			// aapt resource value: 0x7f0b007f
 			public const int text = 2131427455;
@@ -1978,11 +1978,8 @@ namespace HelloGridView
 			// aapt resource value: 0x7f0b0058
 			public const int txtRatings = 2131427416;
 			
-			// aapt resource value: 0x7f0b0085
-			public const int txtReviewComments = 2131427461;
-			
 			// aapt resource value: 0x7f0b0084
-			public const int txtReviewCommentsLabel = 2131427460;
+			public const int txtReviewComments = 2131427460;
 			
 			// aapt resource value: 0x7f0b0057
 			public const int txtVintage = 2131427415;
@@ -2131,61 +2128,58 @@ namespace HelloGridView
 			public const int ExploreFragment = 2130903069;
 			
 			// aapt resource value: 0x7f03001e
-			public const int header = 2130903070;
+			public const int LayoutFtagment = 2130903070;
 			
 			// aapt resource value: 0x7f03001f
-			public const int LayoutFtagment = 2130903071;
+			public const int Main = 2130903071;
 			
 			// aapt resource value: 0x7f030020
-			public const int Main = 2130903072;
+			public const int notification_media_action = 2130903072;
 			
 			// aapt resource value: 0x7f030021
-			public const int notification_media_action = 2130903073;
+			public const int notification_media_cancel_action = 2130903073;
 			
 			// aapt resource value: 0x7f030022
-			public const int notification_media_cancel_action = 2130903074;
+			public const int notification_template_big_media = 2130903074;
 			
 			// aapt resource value: 0x7f030023
-			public const int notification_template_big_media = 2130903075;
+			public const int notification_template_big_media_narrow = 2130903075;
 			
 			// aapt resource value: 0x7f030024
-			public const int notification_template_big_media_narrow = 2130903076;
+			public const int notification_template_lines = 2130903076;
 			
 			// aapt resource value: 0x7f030025
-			public const int notification_template_lines = 2130903077;
+			public const int notification_template_media = 2130903077;
 			
 			// aapt resource value: 0x7f030026
-			public const int notification_template_media = 2130903078;
+			public const int notification_template_part_chronometer = 2130903078;
 			
 			// aapt resource value: 0x7f030027
-			public const int notification_template_part_chronometer = 2130903079;
+			public const int notification_template_part_time = 2130903079;
 			
 			// aapt resource value: 0x7f030028
-			public const int notification_template_part_time = 2130903080;
+			public const int psts_tab = 2130903080;
 			
 			// aapt resource value: 0x7f030029
-			public const int psts_tab = 2130903081;
+			public const int ReviewPopup = 2130903081;
 			
 			// aapt resource value: 0x7f03002a
-			public const int ReviewPopup = 2130903082;
+			public const int select_dialog_item_material = 2130903082;
 			
 			// aapt resource value: 0x7f03002b
-			public const int select_dialog_item_material = 2130903083;
+			public const int select_dialog_multichoice_material = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int select_dialog_multichoice_material = 2130903084;
+			public const int select_dialog_singlechoice_material = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int select_dialog_singlechoice_material = 2130903085;
+			public const int support_simple_spinner_dropdown_item = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int support_simple_spinner_dropdown_item = 2130903086;
+			public const int Tabs = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int Tabs = 2130903087;
-			
-			// aapt resource value: 0x7f030030
-			public const int TasteFragment = 2130903088;
+			public const int TasteFragment = 2130903087;
 			
 			static Layout()
 			{
